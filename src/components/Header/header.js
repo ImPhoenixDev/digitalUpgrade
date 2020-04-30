ce04840b1bb784d36d9ba1290f0f5fea8e8ed049
MrLag <emo30000@gmail.com>
2020-04-30 17:46:25 -0500

    [style] Header: More height.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -8,7 +8,7 @@ import { faMousePointer } from "@fortawesome/free-solid-svg-icons"
 import "./Header.styl"
 
 const Header = ({ siteTitle }) => (
-  <header className="flex justify-between w-full h-12 lg:h-11 grid-cols-12 probando border-gray-111 border-opacity-25 border-solid border-b items-center">
+  <header className="flex justify-between w-full h-12 lg:h-13 grid-cols-12 probando border-gray-111 border-opacity-25 border-solid border-b items-center">
     <div className="brand flex items-center">
       <FontAwesomeIcon icon={faMousePointer} />
       <h1 className="font-bold mx-4"> Digital Upgrade</h1>
